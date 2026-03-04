99a554769506f67453b0c8ab4feeac0ca2c47108
TINIL1N <98623221+TINIL1N@users.noreply.github.com>
2026-03-04 20:07:48 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-// возраст
 const birthDate = new Date("2010-02-03")
 const today = new Date()
 
 let age = today.getFullYear() - birthDate.getFullYear()
-
 const m = today.getMonth() - birthDate.getMonth()
 
 if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
 age--
 }
 
-document.getElementById("age").textContent = age
+const ageElement = document.getElementById("age")
+if(ageElement){
+ageElement.textContent = age
+}
 
-
-// FIDE рейтинги
 fetch("fide.json")
 .then(r => r.json())
 .then(data => {
 
-document.getElementById("fideStandard").textContent = data.standard
-document.getElementById("fideRapid").textContent = data.rapid
-document.getElementById("fideBlitz").textContent = data.blitz
+const std = document.getElementById("fideStandard")
+const rapid = document.getElementById("fideRapid")
+const blitz = document.getElementById("fideBlitz")
+
+if(std) std.textContent = data.standard
+if(rapid) rapid.textContent = data.rapid
+if(blitz) blitz.textContent = data.blitz
 
 })
-.catch(e => console.log(e))
+.catch(e => console.log("Ошибка загрузки FIDE:", e))
 
 })
