b8d59dad4d5ce5b030485fafe6b89144fd91e8aa
TINIL1N <98623221+TINIL1N@users.noreply.github.com>
2026-03-04 17:27:19 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+// дата рождения
+const birthDate = new Date("2010-02-03")
+
+const today = new Date()
+
+let age = today.getFullYear() - birthDate.getFullYear()
+
+const m = today.getMonth() - birthDate.getMonth()
+
+if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+age--
+}
+
+document.getElementById("age").textContent = age
+
+
+
+// загрузка рейтингов FIDE
+
+fetch("fide.json")
+.then(response => response.json())
+.then(data => {
+
+document.getElementById("fideStandard").textContent = data.standard
+
+document.getElementById("fideRapid").textContent = data.rapid
+
+document.getElementById("fideBlitz").textContent = data.blitz
+
+})
