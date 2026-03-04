1097d0789936170d737d49e14d402bbe2b3b3110
TINIL1N <98623221+TINIL1N@users.noreply.github.com>
2026-03-04 18:22:25 +0500

    Update script.js

--- script.js
+++ script.js
@@ -28,3 +28,16 @@ document.getElementById("fideRapid").textContent = data.rapid
 document.getElementById("fideBlitz").textContent = data.blitz
 
 })
+
+// дата обновления рейтингов
+
+const now = new Date()
+
+const options = {
+year: "numeric",
+month: "long",
+day: "numeric"
+}
+
+document.getElementById("updateDate").textContent =
+now.toLocaleDateString("ru-RU", options)
